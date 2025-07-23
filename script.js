3f1050c1515694dffb2411212d8b899464997c61
Alipsagira <157944760+Alipsagira@users.noreply.github.com>
2025-07-23 06:34:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 
 
-// Handle lessons that aren't cached
 function showOfflineAlert(lessonName) {
-  const message = `${lessonName} is not available offline. Please connect to the internet.`;
-  alert(message);
+  alert(`${lessonName} is not available offline. Please connect to the internet.`);
 }
 
-// Attach event listeners to fallback links
+// Attach click event to offline lessons
 document.querySelectorAll('[data-lesson]').forEach(link => {
-  link.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent link default
-    const lesson = event.target.dataset.lesson;
-    showOfflineAlert(lesson);
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const lessonName = event.target.dataset.lesson;
+    showOfflineAlert(lessonName);
 });
 });
